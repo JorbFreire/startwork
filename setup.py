696e4878eca1ac270644c76fcd3b997012010418
JorbFreire <jorbfreire@gmail.com>
2023-03-11 20:33:49 -0300

    update to alpha

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     install_requires=["inquirer==3.1.2"],
     keywords=['python', 'inquirer', 'cli', 'dev-tools', 'tooling'],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
