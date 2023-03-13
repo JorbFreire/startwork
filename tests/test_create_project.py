31f6f2c766d0855605f88efce3817940155a4d04
JorbFreire <jorbfreire@gmail.com>
2023-03-13 06:23:07 -0300

    mock prompt and fight with fixtures

--- tests/test_create_project.py
+++ tests/test_create_project.py
@@ -1,15 +1,57 @@
 import pytest
 from pathlib import Path
 from startwork.actions.create_project import create_project
+from inquirer import prompt, Text
+import unittest, inquirer
+import json
 
+from unittest.mock import patch
 
-class TestCreateProject:
+prompt_mock_value = [
+  {"name": "sample_name", "project_path": "/home"},
+  {"name": "sample_name2", "project_path": "/home"},
+  {"name": "sample_name3", "project_path": "/home"},
+]
+
+class TestCreateProject(unittest.TestCase):
+  # CLASS SETUP
   project_list_path = Path(__file__).parent / "projects_list.json"
-  
+
+  @pytest.fixture(autouse=True)
+  def _capsys(self, capsys):
+    self.capsys = capsys
+
+  @patch(
+    "startwork.actions.create_project.prompt",
+    return_value=prompt_mock_value[0]
+  )
+  def _happy_path(self, mock_inquirer_prompt, mock_index):
+    create_project(self.project_list_path)
+    out, err = self.capsys.readouterr()
+    assert out == "New project created!\n"
+    assert err == ''
+
+    test_list = []
+    with open(self.project_list_path, "r") as file:
+      test_list = json.load(file)
+
+    assert test_list == prompt_mock_value[mock_index]
+
+
+  # TESTS
   def test_if_is_function(self):
     assert callable(create_project)
 
-  @pytest.fixture(params=['nodict', 'dict'])
-  def test_happy_path(self):
-    create_project(self.project_list_path)
-    assert True
+  @pytest.mark.parametrize("mock_index", (0, 1, 2))
+  def test_happy_path(self, mock_index):
+    self._happy_path(mock_index)
+
+  # @patch(
+  #   "startwork.actions.create_project.prompt",
+  #   return_value=prompt_mock_value[0]
+  # )
+  # def test_repeated_name(self, mock_inquirer_prompt):
+  #   create_project(self.project_list_path)
+  #   out, err = self.capsys.readouterr()
+  #   assert out == ""
+  #   assert err == f'Name "{mock_inquirer_prompt.name}" alredy in use'
