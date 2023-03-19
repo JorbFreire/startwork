172dd16c7dc03bf6280e2723079211150df9d4b5
JorbFreire <jorbfreire@gmail.com>
2023-03-19 15:57:55 -0300

    test confirmation boundary

--- tests/test_DeleteProject.py
+++ tests/test_DeleteProject.py
@@ -9,7 +9,6 @@ import json
 from startwork.models.CreateProject import CreateProject
 from startwork.models.DeleteProject import DeleteProject
 
-from pprint import pprint
 
 mock_values_create = [
   {"name": "sample_name1", "project_path": "/home"},
@@ -23,10 +22,29 @@ mock_values_delete = [{
   "delete_confirmed2": True,
 } for value_create in mock_values_create ]
 
+mock_values_delete_unconfirmed = [
+  {
+    "selected_project": mock_values_create[0]["name"],
+    "delete_confirmed": False,
+    "delete_confirmed2": False,
+  },
+  {
+    "selected_project": mock_values_create[0]["name"],
+    "delete_confirmed": False,
+    "delete_confirmed2": True,
+  },
+  {
+    "selected_project": mock_values_create[0]["name"],
+    "delete_confirmed": True,
+    "delete_confirmed2": False,
+  }
+]
+
 class TestDeleteProject(unittest.TestCase):
   # CLASS SETUP
   project_list_path = Path(__file__).parent / "projects_list.json"
   mock_values_create = deepcopy(mock_values_create)
+  generic_error_exception = Exception("Deletion Aborted!")
 
   @pytest.fixture(autouse=True)
   def _capsys(self, capsys):
@@ -83,3 +101,39 @@ class TestDeleteProject(unittest.TestCase):
         )
         result = json.load(file)
         assert result == expected_result
+
+
+  @patch(
+    "startwork.models.CreateProject.prompt",
+    return_value=mock_values_create[0]
+  )
+  @patch(
+    "startwork.models.DeleteProject.prompt",
+    return_value=mock_values_delete_unconfirmed[0]
+  )
+  def test_delete_unconfirmed(
+    self,
+    mock_inquirer_prompt_delete,
+    mock_inquirer_prompt_create
+  ):
+    CreateProject.run(self.project_list_path)
+    with open(self.project_list_path, "r") as file:
+      result = json.load(file)
+      assert result == [mock_values_create[0]]
+    # clean output
+    self.capsys.readouterr()
+
+    for mock_value in mock_values_delete_unconfirmed:
+      mock_inquirer_prompt_delete.return_value["delete_confirmed"] = mock_value["delete_confirmed"]
+      mock_inquirer_prompt_delete.return_value["delete_confirmed2"] = mock_value["delete_confirmed2"]
+      try:
+        DeleteProject.run(self.project_list_path)
+        assert False
+      except Exception as exception:
+        assert exception.args == self.generic_error_exception.args
+
+    with open(self.project_list_path, "w") as outfile:
+      outfile.write(json.dumps([], indent=4))
+
+    with open(self.project_list_path, "r") as file:
+      assert json.load(file) == []
